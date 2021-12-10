08f1f87375563a8b56c8125f1162088733845ebb
Zara Cooper <zramcooper@gmail.com>
2021-12-10 14:50:12 +0300

    [Step 3] Create Home Component

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PortfolioRoutingModule } from './portfolio-routing.module';
-import { PortfolioComponent } from './portfolio.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    PortfolioComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
