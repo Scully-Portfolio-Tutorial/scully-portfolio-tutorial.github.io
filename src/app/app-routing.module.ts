d59a4122dd7b96a1fcd097ce9aabdff09533cc5e
Zara Cooper <zramcooper@gmail.com>
2021-12-10 14:46:07 +0300

    [Step 3] Change path to PortfolioModule In AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) }];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
